db51bb99995cbfcedd361cbf90857c4a73ff47ac
Thomas Pyle <thomasp162@gmail.com>
2017-11-17 17:11:30 -0500

    Added comment methods

--- data/comments.js
+++ data/comments.js
@@ -7,19 +7,68 @@ const exported_methods = {
             throw `getCommentById: Expected a string but received a ${typeof ( id )}`;
         }
         const commentCollection = await comments();
-        const data = await commentCollection.find ( { _id: id } );
-
+        const data = await commentCollection.findOne ( { _id: id } );
+        return data;
+    },
+    async updateCommentById ( id, comment ) {
+        if ( typeof ( id ) !== 'string' ) {
+            throw `updateCommentById: Expected a string but received a ${typeof ( id )}`;
+        } 
+        update = {};
+        update.comment = comment;
+        const commentCollection = await comments();
+        await commentCollection.updateOne ( { _id: id }, { $set: update } );
+        
+    },
+    async removeCommentById ( id ) {
+        if ( typeof ( id ) !== 'string' ) {
+            throw `removeCommentById: Expected a string but received a ${typeof(id)}`;
+        }
+        const commentCollection = await comments();
+        let data = await commentCollection.removeOne ( { _id: id } );
+        return data;
+    },
+    async getCommentsByUserId ( userId ) {
+        if ( typeof ( id ) !== 'string' ) {
+            throw `getCommentById: Expected a string but received a ${typeof ( id )}`;
+        }
+        const commentCollection = await comments();
+        const data = await commentCollection.find ( { userId: userId } ).toArray();
+        return data;
+    },
+    async removeCommentsByUserId ( userId ) {
+        if ( typeof ( id ) !== 'string' ) {
+            throw `removeCommentById: Expected a string but received a ${typeof(id)}`;
+        }
+        const commentCollection = await comments();
+        let data = await commentCollection.remove ( { userId: userId } );
+        return data;
+    },
+    async getCommentsByRecipeId ( recipeId ) {
+        if ( typeof ( id ) !== 'string' ) {
+            throw `getCommentById: Expected a string but received a ${typeof ( id )}`;
+        }
+        const commentCollection = await comments();
+        const data = await commentCollection.find ( { recipeId: recipeId } ).toArray();
+        return data;
+    },
+    async removeCommentsByRecipeId ( recipeId ) {
+        if ( typeof ( id ) !== 'string' ) {
+            throw `removeCommentById: Expected a string but received a ${typeof(id)}`;
+        }
+        const commentCollection = await comments();
+        let data = await commentCollection.remove ( { recipeId: recipeId } );
         return data;
     },
     async createComment ( userId, recipeId, comment ) {
         if ( typeof ( userId ) !== 'string' ) {
-            throw `getCommentById: Expected a string but received a ${typeof ( userId )}`;
+            throw `createComment: Expected a string but received a ${typeof ( userId )}`;
         }
         if ( typeof ( recipeId ) !== 'string' ) {
-            throw `getCommentById: Expected a string but received a ${typeof ( recipeId )}`;
+            throw `createComment: Expected a string but received a ${typeof ( recipeId )}`;
         }
         if ( typeof ( comment ) !== 'string' ) {
-            throw `getCommentById: Expected a string but received a ${typeof ( comment )}`;
+            throw `createComment: Expected a string but received a ${typeof ( comment )}`;
         }
 
         comBody = {};
