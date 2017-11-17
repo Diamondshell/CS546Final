dfe396dab10968dfb039fa4a3a4afdb426227082
Thomas Pyle <thomasp162@gmail.com>
2017-11-17 17:13:05 -0500

    Added test file

---
+++ test.js
@@ -0,0 +1,12 @@
+var data = require ( "./data" );
+const comments = data.comments;
+
+function main() {
+    console.log("Comment Tests");
+    let t1 = await comments.createComment("a","b","lol");
+    console.log("Testing Create: " + t1);
+    let t2 = await comments.getCommentById(t1._id);
+    console.log("Testing Get: " + t2 );
+    let t3 = await comments.removeCommentById(t1._id);
+    console.log("Testing Remove: " + t3);
+}
