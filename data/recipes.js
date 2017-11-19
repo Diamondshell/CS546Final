145ed5505a170cdf9c0cfe3fedfa4f8db5f19529
Thomas Pyle <thomasp162@gmail.com>
2017-11-19 15:24:45 -0500

    Created base code for each database js file

---
+++ data/recipes.js
@@ -0,0 +1,9 @@
+const mongoCollections = require ( "../config/mongoCollections" );
+const recipes = mongoCollections.recipeItems;
+const uuidv1 = require ( "uuid/v1" );
+
+const exported_methods = {
+
+};
+
+module.exports = exported_methods;
---
+++ data/users.js
@@ -0,0 +1,9 @@
+const mongoCollections = require ( "../config/mongoCollections" );
+const users = mongoCollections.userItems;
+const uuidv1 = require ( "uuid/v1" );
+
+const exported_methods = {
+
+};
+
+module.exports = exported_methods;
