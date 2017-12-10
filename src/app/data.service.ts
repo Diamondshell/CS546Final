816589e735beb1d7a006660e2be733511de56228
mknapp19 <knappm@optonline.net>
2017-12-10 13:31:28 -0500

    Added http request layout

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,25 +5,32 @@ import { RecipeDetail } from './recipeDetails';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import {recipes, profileInfo, recipeDetail } from './mock-data';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @Injectable()
 export class DataService {
 
+  constructor(private _http: Http) { }
+
   getAllRecipes():Observable<Recipe[]>{
-      return of(recipes);
+    return this._http.get("/").map(result => this.result = result.json().data);
+    // return of(recipes);
   }
 
   getCurrentUser():Observable<User>{
-    return of(profileInfo);
+    return this._http.get("/").map(result => this.result = result.json().data);
+    // return of(profileInfo);
   }
   getRecipeById(id):Observable<RecipeDetail>{
-    return of(recipeDetail);
+    return this._http.get("/", id).map(result => this.result = result.json().data);
+    // return of(recipeDetail);
   }
   updateUserInfo(changed){
     //send changed data to server
+    this._http.post("/", changed);
     console.log(changed);
     return of("");
   }
-
-  constructor() { }
 
 }
